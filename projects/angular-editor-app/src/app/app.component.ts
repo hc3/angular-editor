2e6cb64ca634e877c18ca874bd9c21cc238d0ef0
Eliel <zelimen741@gmail.com>
2021-11-04 17:12:00 -0300

    change path to angularconfig

--- projects/angular-editor-app/src/app/app.component.ts
+++ projects/angular-editor-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AngularEditorConfig} from 'angular-editor';
+import {AngularEditorConfig} from '../../../angular-editor/src/lib/config';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
